2ada3a2f3b74d82672c2adb6937d7a05c2475fea
wizardBr <florentino.flavioweb@gmail.com>
2020-01-13 15:00:11 -0300

    feat(events, lists): Listening to events ans redenring lists

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [ enteredGoal, setEnteredGoal ] = useState('');
+  const [ courseGoals, setCourseGoals ] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+   setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.screen}>
 
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Add your goal"
-          style={styles.input} />
-        <Button title="ADD" />
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
 
       <View>
-
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
 
     </View>
